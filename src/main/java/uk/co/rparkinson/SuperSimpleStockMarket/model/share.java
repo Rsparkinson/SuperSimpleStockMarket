72a45b336f48c6a49c9935513bcf36de55d5c6f1
Rsparkinson <info@rparkinson.co.uk>
2016-08-22 20:17:35 +0100

    Added Model for Share and Trade.
    Created pom.xml

---
+++ src/main/java/uk/co/rparkinson/SuperSimpleStockMarket/model/share.java
@@ -0,0 +1,78 @@
+package uk.co.rparkinson.SuperSimpleStockMarket.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class share {
+
+    private int c_id;
+    private String c_symbol;
+    private String c_type; // Common || Preferred
+    private float c_lastDividend;
+    private float c_fixedDividend;
+    private float c_parValue;
+
+    public share() {
+    }
+
+    public int getId() {
+      return c_id;
+    }
+
+    public void setId(String p_id) {
+      c_id = p_id;
+    }
+
+    public String getSymbol() {
+      return c_symbol;
+    }
+
+    public void setSymbol(String p_symbol) {
+      c_symbol = p_symbol;
+    }
+
+    public String getType() {
+      return c_type;
+    }
+
+    public void setType(String p_type) {
+      if (p_type.equalsignorecase("Preferred")) {
+        c_type = "Preferred";
+      } else {
+        c_type = "Common";
+      }
+    }
+
+    public Float getLastDividend() {
+      return c_lastDividend;
+    }
+
+    public void setLastDividend(float p_lastDividend) {
+      c_lastDividend = p_lastDividend;
+    }
+
+    public Float getFixedDividend() {
+      return c_fixedDividend;
+    }
+
+    public void setFixedDividend(float p_fixedDividend) {
+      c_fixedDividend = p_fixedDividend;
+    }
+
+    public Float getParValue() {
+      return c_parValue;
+    }
+
+    public void setParValue(float p_parValue) {
+      c_parValue = p_parValue;
+    }
+
+    public String toString() {
+      return "id:" + c_id + " symbol:" + c_symbol + " type:" + c_type
+        + " lastDividend:" + c_lastDividend + " fixedDividend:"
+        + c_fixedDividend + " parValue:" + c_parValue;
+    }
+}
---
+++ src/main/java/uk/co/rparkinson/SuperSimpleStockMarket/model/trade.java
@@ -0,0 +1,71 @@
+package uk.co.rparkinson.SpringTest.model;
+
+@Entity
+public class trade {
+    private int c_id;
+    private long c_timeStamp;
+    private String c_symbol;
+    private int c_qty;
+    private String c_type; //buy || sell
+    private float c_tradePrice;
+
+    public trade() {
+    }
+
+    public int getId() {
+      return c_id;
+    }
+
+    public void setId(int p_id) {
+      c_id = p_id;
+    }
+
+    public long getTimeStamp() {
+      return c_timeStamp;
+    }
+
+    public void setTimeStamp(long p_timeStamp) {
+      c_timeStamp = p_timeStamp;
+    }
+
+    public String getSymbol() {
+      return c_symbol;
+    }
+
+    public void setSymbol(String p_symbol) {
+      c_symbol = p_symbol;
+    }
+
+    public int getQty() {
+      return c_qty;
+    }
+
+    public void setQty(int p_qty) {
+      c_qty = p_qty;
+    }
+
+    public String getType() {
+      return c_type;
+    }
+
+    public void setType(String p_type) {
+      if (p_type.equalsignorecase("buy")) {
+        c_type = "buy";
+      } else {
+        c_type = "sell";
+      }
+    }
+
+    public Float getTradePrice() {
+      return c_tradePrice;
+    }
+
+    public void setTradePrice(float p_tradePrice) {
+      c_tradePrice = p_tradePrice;
+    }
+
+    public String toString() {
+      return "id:" + c_id + " symbol:" + c_symbol + " timeStamp:" + c_timeStamp + " qty:" + c_qty
+        + " type:" + c_type + " tradePrice:" + c_tradePrice;
+    }
+}
