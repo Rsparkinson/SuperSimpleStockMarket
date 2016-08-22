9b75dcc7228880fefba945139c06136a0dee0abf
Rsparkinson <info@rparkinson.co.uk>
2016-08-23 00:16:05 +0100

    Add assignment question

--- src/main/java/uk/co/rparkinson/SuperSimpleStockMarket/controller/MarketController.java
+++ src/main/java/uk/co/rparkinson/SuperSimpleStockMarket/controller/MarketController.java
@@ -15,6 +15,8 @@ import org.slf4j.LoggerFactory;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 @Controller
 public class MarketController {
@@ -27,31 +29,87 @@ public class MarketController {
   @ResponseBody
   @RequestMapping("/")
   String home() {
+      String t_text = "<p> SuperSimpleStockMarket </p>";
+      MarketService.loadData();
 
-      //MarketService.loadData();
+      t_text = t_text + "<p>Example Assignment – Super Simple Stock Market </p>";
+      t_text = t_text + "<p>Requirements </p>";
+      t_text = t_text + "<p>  1. Provide working source code that will :- </p>";
+      t_text = t_text + "<p>    i. Given any price as input, calculate the dividend yield </p>";
 
-      //Collection<share> r_shares = MarketService.findAll();
+        String t_symbol = "GIN";
+        float t_price = 123.65f;
+        float t_dividend = 0f;
 
-    // //i. Given any price as input, calculate the dividend yield
-    //     String t_symbol = "TEA";
-    //     float t_price = 123.65f;
-    //     float t_result = 0f;
-    //
-    //     share t_share = c_ShareService.find(t_symbol);
-    //
-    //     if (t_share.getType().equals("Common")) {
-    //       float t_lastDividend = t_share.getLastDividend();
-    //       t_result = c_CalcService.dividendYieldCommon(t_lastDividend, t_price);
-    //     }
-    //
-    //     if (t_share.getType().equals("Preferred")) {
-    //       float t_fixedDividend = t_share.getFixedDividend();
-    //       float t_parValue = t_share.getParValue();
-    //       t_result = c_CalcService.dividendYieldPreferred(t_fixedDividend, t_parValue, t_price);
-    //     }
-    //
-    //     t_HTMLresults = "Result = " + t_result;
-      return "SuperSimpleStockMarket";
+        share t_share = MarketService.findShare(t_symbol);
+
+        if (t_share.getType().equals("Common")) {
+          float t_lastDividend = t_share.getLastDividend();
+          t_dividend = MarketService.dividendYieldCommon(t_lastDividend, t_price);
+        }
+
+        if (t_share.getType().equals("Preferred")) {
+          float t_fixedDividend = t_share.getFixedDividend();
+          float t_parValue = t_share.getParValue();
+          t_dividend = MarketService.dividendYieldPreferred(t_fixedDividend, t_parValue, t_price);
+        }
+        t_text = t_text + "<p>    Share " + t_symbol + " Price " + t_price + " dividend yield " + t_dividend + " </p>";
+
+        t_text = t_text + "<p>     </p>";
+        t_text = t_text + "<p>ii. Given any price as input, calculate the P/E Ratio     </p>";
+
+        t_symbol = "GIN";
+        t_price = 123.65f;
+        float t_PEratio = 0f;
+
+        t_PEratio = MarketService.dividendYieldCommon(t_price, t_dividend);
+
+        t_text = t_text + "<p>    Share " + t_symbol + " Price " + t_price + " dividend yield " + t_dividend + " P/E Ratio " + t_PEratio + " </p>";
+
+        t_text = t_text + "<p>     </p>";
+        t_text = t_text + "<p>    iii. Record a trade, with timestamp, quantity of shares, buy or sell indicator and traded price </p>";
+
+        trade t_trade = new trade();
+        t_trade.setSymbol("TEA");
+        t_trade.setQty(10);
+        t_trade.setType("Buy");
+        t_trade.setTradePrice(12.12f);
+
+        trade t_returnTrade = MarketService.createTrade(t_trade);
+
+        Date t_date_TimeStamp = new Date(t_returnTrade.getTimeStamp());
+        SimpleDateFormat dateformatter = new SimpleDateFormat("dd/mm/yyyy HH:mm");
+
+        t_text = t_text + "<p>    Trade records at " + dateformatter.format(t_date_TimeStamp) + " Share " + t_trade.getSymbol() + " Qty " + t_trade.getQty() + "  Type " + t_trade.getType() + " Price " +t_trade.getTradePrice()+ "</p>";
+
+        t_text = t_text + "<p>     </p>";
+        t_text = t_text + "<p>    iv. Calculate Volume Weighted Stock Price based on trades in past 15 minutes</p>";
+
+        Collection<trade> t_15minTrades = MarketService.findLast15minsTrades();
+
+        int t_sampleSize = t_15minTrades.size();
+        int[] t_15minQty = new int[t_sampleSize];
+        float[] t_15minPrices = new float[t_sampleSize];
+
+        t_text = t_text + "<p>     </p>";
+        int i = 0;
+        for (trade t_15minTrade : t_15minTrades) {
+          t_text = t_text + "<p>     " + t_15minTrade.toString() + "</p>";
+          t_15minQty[i] = t_15minTrade.getQty();
+          t_15minPrices[i] = t_15minTrade.getTradePrice();
+          i++;
+        }
+
+        float t_volumeWeightStockPrice = MarketService.volumeWeightStockPrice(t_15minPrices, t_15minQty);
+
+        t_text = t_text + "<p>    Volume Weight Stock Price  " + t_volumeWeightStockPrice + "</p>";
+
+        t_text = t_text + "<p>     </p>";
+        t_text = t_text + "<p>    b. Calculate the GBCE All Share Index using the geometric mean of prices for all stocks </p>";
+        t_text = t_text + "<p>     </p>";
+        t_text = t_text + "<p>     </p>";
+
+      return t_text;
   }
 
 }
